f059eee441625ce4b24e59764f9432611261a9b9
bagusnegara <92941645+bagusnegara@users.noreply.github.com>
2021-10-21 23:49:16 +0700

    Create getASCII.js
    Get ASCII value of a character.

---
+++ getASCII.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Get ASCII value of a character.
+ * For the ASCII table you can refer to http://www.asciitable.com/
+ */
+
+
+function getASCII(char){
+
+  return char.charCodeAt();
+}
+
+console.log(getASCII("8");
