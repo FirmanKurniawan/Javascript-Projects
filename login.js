4c05695c8f963e224a790409c01226a38b18748d
Arizki2002 <93144192+Arizki2002@users.noreply.github.com>
2021-10-25 22:35:54 +0700

    Create login.js
    hacktoberfest

---
+++ login.js
@@ -0,0 +1,99 @@
+         desc: Lang.queryJS('login.error.authDown.desc')
+                    }
+                }
+            }
+        }
+    }
+    if(err.message != null){
+        if(err.message === 'NotPaidAccount'){
+            return {
+                title: Lang.queryJS('login.error.notPaid.title'),
+                desc: Lang.queryJS('login.error.notPaid.desc')
+            }
+        } else {
+            // Unknown error with request.
+            return {
+                title: Lang.queryJS('login.error.unknown.title'),
+                desc: err.message
+            }
+        }
+    } else {
+        // Unknown Mojang error.
+        return {
+            title: err.error,
+            desc: err.errorMessage
+        }
+    }
+}
+
+let loginViewOnSuccess = VIEWS.landing
+let loginViewOnCancel = VIEWS.settings
+let loginViewCancelHandler
+
+function loginCancelEnabled(val){
+    if(val){
+        $(loginCancelContainer).show()
+    } else {
+        $(loginCancelContainer).hide()
+    }
+}
+
+loginCancelButton.onclick = (e) => {
+    switchView(getCurrentView(), loginViewOnCancel, 500, 500, () => {
+        loginUsername.value = ''
+        loginPassword.value = ''
+        loginCancelEnabled(false)
+        if(loginViewCancelHandler != null){
+            loginViewCancelHandler()
+            loginViewCancelHandler = null
+        }
+    })
+}
+
+// Disable default form behavior.
+loginForm.onsubmit = () => { return false }
+
+// Bind login button behavior.
+loginButton.addEventListener('click', () => {
+    // Disable form.
+    formDisabled(true)
+
+    // Show loading stuff.
+    loginLoading(true)
+
+    AuthManager.addAccount(loginUsername.value, loginPassword.value).then((value) => {
+        updateSelectedAccount(value)
+        loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.loggingIn'), Lang.queryJS('login.success'))
+        $('.circle-loader').toggleClass('load-complete')
+        $('.checkmark').toggle()
+        setTimeout(() => {
+            switchView(VIEWS.login, loginViewOnSuccess, 500, 500, () => {
+                // Temporary workaround
+                if(loginViewOnSuccess === VIEWS.settings){
+                    prepareSettings()
+                }
+                loginViewOnSuccess = VIEWS.landing // Reset this for good measure.
+                loginCancelEnabled(false) // Reset this for good measure.
+                loginViewCancelHandler = null // Reset this for good measure.
+                loginUsername.value = ''
+                loginPassword.value = ''
+                $('.circle-loader').toggleClass('load-complete')
+                $('.checkmark').toggle()
+                loginLoading(false)
+                loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.success'), Lang.queryJS('login.login'))
+                formDisabled(false)
+            })
+        }, 1000)
+    }).catch((err) => {
+        loginLoading(false)
+        const errF = resolveError(err)
+        setOverlayContent(errF.title, errF.desc, Lang.queryJS('login.tryAgain'))
+        setOverlayHandler(() => {
+            formDisabled(false)
+            toggleOverlay(false)
+        })
+        toggleOverlay(true)
+        loggerLogin.log('Error while logging in.', err)
+    })
+
+})
