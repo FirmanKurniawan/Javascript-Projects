3bef55b0da0f30342929b5be62825bf14e2d300e
Kanika Gupta <55401625+kanika321@users.noreply.github.com>
2021-10-25 17:19:35 +0530

    Create jsbutton.js

---
+++ jsbutton/jsbutton.js
@@ -0,0 +1,10 @@
+$("button").on("click", function() {
+    $(".txtstyle").css({ "font-family": "Brush Script MT " });
+    $(".container").css({ "font-family": "Tahoma", "padding": "0.10px" });
+    $("h2").css({ "color": "#DC143C", "font-size": "40px" })
+    $("body").css({ "background-color": "black", "color": "white" });
+    $("h5").css("font-size", "30px")
+    $("strong").css("color", "#FA8072");
+    $(".fs").css("font-size", "25px")
+    $("table").css({ "width": "50%" })
+});
