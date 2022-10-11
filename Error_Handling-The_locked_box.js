80364738e1c7131fd5e57782cae24e66f9206d03
Vika Joestarze <48416497+xmvkp@users.noreply.github.com>
2022-10-11 23:33:10 +0700

    Create Error_Handling-The_locked_box.js

---
+++ Error_Handling-The_locked_box.js
@@ -0,0 +1,33 @@
+var box = {
+  locked: true,
+  unlock: function() { this.locked = false; },
+  lock: function() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  if(box.locked) box.unlock(); 
+  try{
+    body();
+  }finally{
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+console.log(box.locked);
+// => true
